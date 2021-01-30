32f8014bb6a7ee9c5ff8855d20dd52b1af3c132c
javaj-r <javid.rafiee@gmail.com>
2021-01-30 17:09:08 +0330

    8.JPA Data Modeling with Spring and Hibernate. --> 9.JPA Enumerations

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,9 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    /* TODO: 1/29/2021 Create Difficulty Enum
-     *  private Difficulty difficulty;
-     */
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -29,6 +26,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -116,5 +116,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
