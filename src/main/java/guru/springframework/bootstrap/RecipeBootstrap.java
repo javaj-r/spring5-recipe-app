48b0048c7af9ec437d5bec37d5d973cd72315e2f
javaj-r <javid.rafiee@gmail.com>
2021-02-15 02:11:15 +0330

    11.CRUD Operations with Spring MVC. --> 4.Assignment - Display remaining recipe properties.

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -57,9 +57,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         var directions = "1 Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. "
                 + "Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. "
                 + "(See How to Cut and Peel an Avocado.) Place in a bowl."
-                + "\n" + "2 Mash with a fork: Using a fork, roughly mash the avocado. "
+                + "\n\n" + "2 Mash with a fork: Using a fork, roughly mash the avocado. "
                 + "(Don't overdo it! The guacamole should be a little chunky.)"
-                + "\n" + "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. "
+                + "\n\n" + "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. "
                 + "The acid in the lime juice will provide some balance to the richness of the avocado "
                 + "and will help delay the avocados from turning brown."
                 + "\n" + "Add the chopped onion, cilantro, black pepper, and chiles. "
@@ -69,7 +69,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 + "Start with this recipe and adjust to your taste."
                 + "\n" + "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, "
                 + "add it just before serving."
-                + "\n" + "4 Serve: Serve immediately, or if making a few hours ahead, "
+                + "\n\n" + "4 Serve: Serve immediately, or if making a few hours ahead, "
                 + "place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. "
                 + "(The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve.";
 
@@ -108,17 +108,17 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         // Yummy Tacos
         directions = "1 Prepare a gas or charcoal grill for medium-high, direct heat."
-                + "\n" + "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, "
+                + "\n\n" + "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, "
                 + "oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make"
                 + " a loose paste. Add the chicken to the bowl and toss to coat all over."
                 + "\n" + "Set aside to marinate while the grill heats and you prepare the rest of the toppings."
-                + "\n" + "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer "
+                + "\n\n" + "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer "
                 + "inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes."
-                + "\n" + "4 Warm the tortillas: Place each tortilla on the grill or on a hot, "
+                + "\n\n" + "4 Warm the tortillas: Place each tortilla on the grill or on a hot, "
                 + "dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, "
                 + "turn it with tongs and heat for a few seconds on the other side."
                 + "\n" + "Wrap warmed tortillas in a tea towel to keep them warm until serving."
-                + "\n" + "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. "
+                + "\n\n" + "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. "
                 + "Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. "
                 + "Drizzle with the thinned sour cream. Serve with lime wedges.";
 
